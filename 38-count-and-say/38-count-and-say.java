66d6e8417b9ff35a18d8902321cfdaef917d58ea
Manshi <79921936+manshikashyap@users.noreply.github.com>
2022-06-21 22:12:34 +0530

    Time: 8 ms (51.95%), Space: 41.4 MB (81.84%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -2,15 +2,15 @@ class Solution {
     public String countAndSay(int n) {
         if(n==1)return "1";
         if(n==2)return "11";
-        String s="11";
+        StringBuilder s= new StringBuilder("11");
         for(int i=3;i<=n;i++){
             int c=1;
-            s+="&";
-            String t="";
+            s.append('$');
+            StringBuilder t= new StringBuilder();
             for(int j=1;j<s.length();j++){
                 if(s.charAt(j)!=s.charAt(j-1)){
-                    t+=c;
-                    t+=s.charAt(j-1);
+                    t.append(c);
+                    t.append(s.charAt(j-1));
                     c=1;
                 }
                 else{
@@ -19,6 +19,6 @@ class Solution {
             }
             s=t;
         }
-        return s;
+        return s.toString();
     }
 }
