b6e6b426713dc9fad687516364cb6310a4f68a77
Manshi <79921936+manshikashyap@users.noreply.github.com>
2022-07-03 09:12:19 +0530

    Time: 0 ms (100.00%), Space: 42 MB (23.94%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,36 +1,27 @@
 class Solution {
-    private int findLongest(int[] nums){
-        boolean higher = false;
-        int output1 = nums.length, output2 = nums.length;
-        
-           for(int i = 0;i<nums.length-1;i++){
-            if(nums[i] > nums[i+1] && higher == false){
-                higher = true;
-            }else if(nums[i] < nums[i+1] && higher == true){
-                higher = false;
-            }else {
-                output1--;
+    public int wiggleMaxLength(int[] nums) {
+        boolean mark=false;
+        int m=nums.length;
+        int n=nums.length;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1]&& !mark){
+                mark=true;
             }
+            else if(nums[i]>nums[i+1] && mark){
+                mark=false;
+            }
+            else m--;
         }
-        higher = false;
-       
-                 for(int i = 0;i<nums.length-1;i++){
-            if(nums[i] < nums[i+1] && higher == false){
-                higher = true;
-            }else if(nums[i] > nums[i+1] && higher == true){
-                higher = false;
-            }else {
-                output2--;
+        mark=false;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1] && !mark){
+                mark=true;
             }
-        }    
-            
-       return Math.max(output1,output2) ;
-       
-        
-        
-    }
-    public int wiggleMaxLength(int[] nums){
-      
-        return findLongest(nums);
+            else if(nums[i]<nums[i+1] && mark){
+                mark=false;
+            }
+            else n--;
+        }
+        return Math.max(m,n);
     }
 }
