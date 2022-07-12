2b49bea4e355c2eccce4542beed09224d71970c5
Manshi <79921936+manshikashyap@users.noreply.github.com>
2022-07-12 11:24:46 +0530

    Time: 181 ms (49.22%), Space: 42.2 MB (38.60%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -13,20 +13,21 @@ class Solution {
         int side=sum/4;
         int[] sides=new int[]{side,side,side,side};
         Arrays.sort(arr);
-        return helper(arr,n-1,sides);
+        boolean[] dp= new boolean[n];
+        return helper(arr,n-1,sides,dp);
     }
-        public boolean helper(int[] arr,int i,int[] sides){
+        public boolean helper(int[] arr,int i,int[] sides,boolean[] dp){
             if(i==-1){
                 return sides[0]==0 && sides[1]==0 && sides[2]==0 && sides[3]==0;
             }
-
+            if(dp[i])return dp[i];
             for(int j=0;j<4;j++){
                 if(arr[i]>sides[j])continue;
                 sides[j]-=arr[i];
-                if(helper(arr,i-1,sides))return true;
+                if(helper(arr,i-1,sides,dp))return dp[i]=true;
                 sides[j]+=arr[i];
             }
-            return false;
+            return dp[i]=false;
         }
     
 }
