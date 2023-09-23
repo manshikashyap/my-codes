d02cd4e984cdbef3ed66ced3add65a6a33b0e3d5
Manshi <79921936+manshikashyap@users.noreply.github.com>
2023-09-23 17:04:23 +0530

    Added solution - LeetHub

---
+++ Left most and right most index - GFG/left-most-and-right-most-index.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            long k = Long.parseLong(br.readLine().trim());
+            
+            Solution obj = new Solution();
+            pair ans = obj.indexes(a, k);
+            if (ans.first == -1 && ans.second == -1)
+                System.out.println(-1);
+            else
+                System.out.println(ans.first+" "+ans.second);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+*/
+
+class Solution {
+    
+    public pair indexes(long arr[], long x)
+    {
+        // Your code goes here
+         ArrayList<Long> list=new ArrayList<>();
+         for(long num:arr)list.add(num);
+         long first=list.indexOf(x);
+         long last=list.lastIndexOf(x);
+         pair p= new pair(first,last);
+         return p;
+    }
+}
